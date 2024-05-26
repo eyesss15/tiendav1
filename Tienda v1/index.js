66fb132427780672b72156ca7c43199327f6bd7c
eyesss15 <96083702+eyesss15@users.noreply.github.com>
2024-05-27 01:42:56 +0200

    Add files via upload

---
+++ Tienda v1/index.js
@@ -0,0 +1,14 @@
+const btnCart = document.querySelector('.container-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+
+    
+})
+
+function redirigir () {
+    window.location.href = "https://www.instagram.com/";
+}
+
